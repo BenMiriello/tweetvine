c54941d197e9f2e74170a7e1c3eb1eecda13d954
BenMiriello <benmiriello@gmail.com>
2020-10-13 01:16:07 -0400

    Styling: Rough mock of left half of twitter static landing page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -12,11 +12,22 @@ export default function HomePage() {
   return (
     <>
       { user ? <Nav /> : null }
-      <div>
-        <h1>Home Page</h1>
-        <button onClick={() => history.push('/signup')}>Signup</button>
-        <button onClick={() => history.push('/login')}>Login</button>
+      <div className="h-screen flex">
+        <div className="bg-blue-500 w-1/2 h-full flex flex-col justify-around">
+          <div className="flex justify-center">
+            <div>
+              <div className="mx-auto mb-8"><p className="text-xl font-bold">Follow your interests.</p></div>
+              <div className="mx-auto mb-8"><p className="text-xl font-bold">Hear what other people are talking about.</p></div>
+              <div className="mx-auto"><p className="text-xl font-bold">Join the conversation.</p></div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-blue-900 w-1/2 h-full"></div>
       </div>
     </>
   );
 }
+
+/* <h1>Home Page</h1>
+<button onClick={() => history.push('/signup')}>Signup</button>
+<button onClick={() => history.push('/login')}>Login</button> */
