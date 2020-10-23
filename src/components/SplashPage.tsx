26517e8df66a4ad096f3f67032f4be138c4ae207
BenMiriello <benmiriello@gmail.com>
2020-10-22 23:59:08 -0400

    Style: change shade of blue

--- src/components/SplashPage.tsx
+++ src/components/SplashPage.tsx
@@ -9,7 +9,7 @@ export default function SplashPage() {
 
   return (
     <Page>
-      <div className="bg-blue-500 w-1/2 h-full flex flex-col justify-around p-4">
+      <div className="bg-blue-400 w-1/2 h-full flex flex-col justify-around p-4">
         <div className="flex justify-center">
           <div className="max-w-md">
             <div className="mx-auto mb-8">
@@ -30,11 +30,11 @@ export default function SplashPage() {
             <p className="text-3xl font-bold mt-4 mb-8">See what's happening in the world right now</p>
             <p className="text-lg font-bold">Join {appName} today.</p>
             <div className="w-full flex flex-col flex-wrap justify-center">
-              <button className="my-4 h-10 bg-blue-500 rounded-full font-bold" onClick={() => history.push('/signup')}>
+              <button className="my-4 h-10 bg-blue-400 rounded-full font-bold" onClick={() => history.push('/signup')}>
                 Sign up
               </button>
               <button
-                className="mb-2 h-10 border-2 border-blue-500 rounded-full text-blue-300 font-bold"
+                className="mb-2 h-10 border-2 border-blue-400 rounded-full text-blue-300 font-bold"
                 onClick={() => history.push('/login')}
               >
                 Login
